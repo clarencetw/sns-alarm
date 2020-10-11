487c92a531c845ff7d260a13f50dbec3e49adf5b
Clarence <mr.lin.clarence@gmail.com>
2020-10-12 02:03:08 +0800

    Update aws metric widget for line-notify

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # sns-alarm
+
+## Line Notify 推播
+
+使用前請修改 "your-line-notify-token" 取得方法請參考
+
+- [LINE NOTIFY 運用](https://blog.clarence.tw/2017/12/20/use-line-notify/)
+- [[LINE BOT 好好玩 30 天玩轉 LINE API] 第 19 天：為什麼發訊息都要錢，有不用錢的嗎？](https://blog.clarence.tw/2019/10/05/2020ironman19/)
+
+參考：
+
+- https://github.com/aws-samples/aws-cloudwatch-snapshot-graphs-alert-context
--- index.js
+++ index.js
@@ -1,39 +1,52 @@
-const https = require("https");
-const querystring = require("querystring");
+const AWS = require("aws-sdk");
+const axios = require("axios");
+const FormData = require("form-data");
 
-const post = (hostname, path, accessToken, payload) =>
-  new Promise((resolve, reject) => {
-    const postData = querystring.stringify(payload);
-    const options = {
-      hostname,
-      path,
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Authorization: `Bearer ${accessToken}`,
-        "Content-Length": Buffer.byteLength(postData),
+let widgetDefinition = {
+  MetricWidget: {
+    width: 600,
+    height: 400,
+    start: "-PT3H", // 3 小時的資料
+    end: "PT0H",
+    view: "timeSeries",
+    stacked: false,
+    metrics: [],
+    stat: "Average",
+    yAxis: {
+      left: {
+        min: 0,
+        max: 0,
       },
-    };
+    },
+    period: 60,
+    title: "Snapshot Graphs",
+    annotations: {
+      horizontal: [
+        {
+          color: "#ff6961",
+          label: "Trouble threshold start",
+          value: 0,
+        },
+      ],
+    },
+  },
+};
 
-    const req = https.request(options, (res) => {
-      let buffer = "";
-      console.log(`STATUS: ${res.statusCode}`);
-      console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-      res.setEncoding("utf8");
-      res.on("data", (chunk) => (buffer += chunk));
-      res.on("end", () => {
-        console.log(`BODY: ${buffer}`);
-        resolve({
-          status: res.statusCode,
-          headers: res.headers,
-          body: JSON.parse(buffer),
-        });
-      });
-    });
-    req.on("error", (e) => reject(e.message));
-    req.write(postData);
-    req.end();
+const lineNotify = async (url, accessToken, payload) => {
+  let formData = new FormData();
+  formData.append("message", payload.message);
+  formData.append("imageFile", payload.image, {
+    filename: "CloudWatch.png",
+    contentType: "image/png",
   });
+
+  return await axios.post(url, formData, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      ...formData.getHeaders(),
+    },
+  });
+};
 
 exports.handler = function (event) {
   console.log("event:", JSON.stringify(event, undefined, 2));
@@ -44,13 +57,56 @@ exports.handler = function (event) {
     const SnsMessage = JSON.parse(Message);
     const message = `\nSubject: ${Subject}\nTimestamp: ${Timestamp}\nAlert: ${SnsMessage.Trigger.Namespace}\netric: ${SnsMessage.Trigger.MetricName}\nThreshold: ${SnsMessage.Trigger.Threshold}`;
 
-    return await post(
-      "notify-api.line.me",
-      "/api/notify",
-      "your-line-notify-token",
-      {
-        message,
+    var cloudwatch = new AWS.CloudWatch();
+
+    cloudwatch.getMetricWidgetImage(
+      getWidgetDefinition(SnsMessage.Trigger, SnsMessage),
+      async function (err, data) {
+        if (err) console.log(err, err.stack);
+        else {
+          var image = Buffer.from(data.MetricWidgetImage);
+
+          await lineNotify(
+            "https://notify-api.line.me/api/notify",
+            "your-line-notify-token",
+            {
+              message,
+              image,
+            }
+          );
+        }
       }
     );
   }
 };
+
+function getWidgetDefinition(trigger, message) {
+  var metrics = [];
+  var metric = [
+    trigger.Namespace,
+    trigger.MetricName,
+    trigger.Dimensions[0].name,
+    trigger.Dimensions[0].value,
+  ];
+
+  metrics.push(metric);
+  widgetDefinition.MetricWidget.metrics = metrics;
+  widgetDefinition.MetricWidget.yAxis.left.max = getYMax(
+    message.NewStateReason
+  );
+  widgetDefinition.MetricWidget.title = trigger.MetricName;
+  widgetDefinition.MetricWidget.annotations.horizontal[0].value =
+    trigger.Threshold;
+
+  var mw = JSON.stringify(widgetDefinition.MetricWidget);
+  widgetDefinition.MetricWidget = mw;
+
+  return widgetDefinition;
+}
+
+function getYMax(s) {
+  var regex1 = /\[[0-9]*.[0-9]/;
+  var datapoint = regex1.exec(s)[0].substring(1);
+
+  return datapoint * 3;
+}
